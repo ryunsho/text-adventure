452843d8eb7ee5e0957d4fb70ab16bef8d7233a3
Sho <90221218+ryunsho@users.noreply.github.com>
2025-03-22 20:16:57 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var sentence = "The quick brown fox jumps over the lazy dog.";
+var input;
+var startTime, endTime;
+var timer = false;
+var result;
+
+function setup() {
+    input = select("#input");
+    result = select("#result");
+    input.input(startTimer);
+}
+
+function startTimer() {
+    if (timer === false) {
+        startTime = millis();
+        timer = true;
+    }
+    if (input.value() === sentence) {
+        endTime = millis();
+        speed();
+        input.style("color", "black");
+    }else if (input.value() === sentence.substring(0, input.value().length)) {
+        input.style("color", "black");
+    }else{
+        input.style("color", "red");
+    }
+}
+
+function speed() {
+    var time = (endTime - startTime) / 1000; // time in seconds
+    var wpm = (9 / time) * 60; // 9 words in the sentence, calculates wpm
+    result.html("Your speed is " + wpm.toFixed(2) + " words per minute.");
+    timer = false;
+}
+
+function reset() {
+    input.value("");
+    result.html("");
+    timer = false;
+}
